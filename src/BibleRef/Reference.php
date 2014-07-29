1771495ee6cfbd3eef6cfc14116438754743174d
Filip <hello@filipac.net>
2014-07-29 17:31:01 +0300

    first draft

--- src/BibleRef/Reference.php
+++ src/BibleRef/Reference.php
@@ -10,7 +10,20 @@ class Reference {
     $this->reference = $reference;
   }
 
-  public function getName() {
-    return $this->reference;
+  function getArray() {
+    $book = ['name' => "", 'chapter' => "", 'verses' => []];
+    $parts = preg_split('/\s*:\s*/', trim($this->reference, " ;"));
+    if(isset($parts[0])) {
+      if(preg_match('/\d+\s*$/', $parts[0], $out)) {
+        $book['chapter'] = rtrim($out[0]);
+      }
+      $book['name'] = trim(preg_replace('/\d+\s*$/', "", $parts[0]));
+    }
+    if(isset($parts[1])) {
+      $book['verses'] = preg_split('~\s*,\s*~', $parts[1]);
+    }
+
+    return $book;
   }
+
 }
